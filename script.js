6505980a92e29e890ae0f604b1268b64ace87f09
rp-boxdev <rp.boxdev@gmail.com>
2023-08-01 12:40:32 -0400

    add playable game rounds

--- script.js
+++ script.js
@@ -13,3 +13,37 @@ function getPlayerChoice() {
   let selection = prompt("Rock, paper, or scissors?");
   return selection.toLowerCase();
 }
+
+function playRound(playerSelection, computerSelection) {
+  playerSelection = getPlayerChoice();
+  computerSelection = getComputerChoice();
+  if (playerSelection === computerSelection) {
+    console.log(`You chose: ${playerSelection}`);
+    console.log(`Computer chose: ${computerSelection}`);
+    console.log("It's a tie!");
+  } else if (playerSelection === "rock" && computerSelection === "paper") {
+    console.log(`You chose: ${playerSelection}`);
+    console.log(`Computer chose: ${computerSelection}`);
+    console.log("Computer wins.");
+  } else if (playerSelection === "rock" && computerSelection === "scissors") {
+    console.log(`You chose: ${playerSelection}`);
+    console.log(`Computer chose: ${computerSelection}`);
+    console.log("You win!");
+  } else if (playerSelection === "paper" && computerSelection === "rock") {
+    console.log(`You chose: ${playerSelection}`);
+    console.log(`Computer chose: ${computerSelection}`);
+    console.log("You win!");
+  } else if (playerSelection === "paper" && computerSelection === "scissors") {
+    console.log(`You chose: ${playerSelection}`);
+    console.log(`Computer chose: ${computerSelection}`);
+    console.log("Computer wins.");
+  } else if (playerSelection === "scissors" && computerSelection === "rock") {
+    console.log(`You chose: ${playerSelection}`);
+    console.log(`Computer chose: ${computerSelection}`);
+    console.log("Computer wins.");
+  } else if (playerSelection === "scissors" && computerSelection === "paper") {
+    console.log(`You chose: ${playerSelection}`);
+    console.log(`Computer chose: ${computerSelection}`);
+    console.log("You win!");
+  }
+}
